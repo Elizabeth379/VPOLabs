c8f4799d6239ea0f1f80d3cb8323709966214c68
Elizabeth <bellator.379@gmail.com>
2023-09-20 19:43:56 +0300

    Test for searching function created.

---
+++ l1t5/tests/tests.py
@@ -0,0 +1,33 @@
+import unittest
+from unittest.mock import patch
+import os
+import glob
+from main import search_files_with_extension, searching_time
+
+class TestFindFilesWithExtension(unittest.TestCase):
+    def test_search_files_with_extension(self):
+        # Устанавливаем ожидаемый путь к текущей директории
+        expected_current_dir = "C:/projects/vpo/l1t5"
+
+        # Устанавливаем ожидаемое расширение файла
+        expected_extension = "html"
+
+        # Устанавливаем ожидаемый паттерн для поиска файлов
+        expected_search_pattern = os.path.join(expected_current_dir, f"**/*.{expected_extension}")
+
+        # Устанавливаем ожидаемый список файлов, которые должны быть найдены
+        expected_files = ["C:/projects/vpo/l1t5/fortest.html", "C:/projects/vpo/l1t5/subfolder/fortest2.html"]
+
+        # Имитируем glob.glob и возвращаем ожидаемый список файлов
+        with patch('glob.glob', return_value=expected_files) as mock_glob:
+            found_files = search_files_with_extension(expected_extension)
+
+            # Проверяем, что функция glob.glob была вызвана с ожидаемым паттерном
+            mock_glob.assert_called_once_with(expected_search_pattern, recursive=True)
+
+            # Проверяем, что список найденных файлов совпадает с ожидаемым списком
+            self.assertEqual(found_files, expected_files)
+
+
+if __name__ == '__main__':
+    unittest.main()
