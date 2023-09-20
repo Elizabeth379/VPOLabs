0aef706b3c98944187ae7f53fdf7f138300714e2
Elizabeth <bellator.379@gmail.com>
2023-09-20 18:56:38 +0300

    Gradient table refactored. Tests created.

--- l1t4/main.py
+++ l1t4/main.py
@@ -2,7 +2,7 @@ def generate_html_table(rows, output_file):
     # Открываем файл для записи HTML
     with open(output_file, 'w') as file:
         # Записываем начало HTML-файла и начало таблицы
-        file.write("<html><head></head><body><table>")
+        file.write("<html><head></head><body><table style='border-spacing: 0; width: 80vw; heigth: 10vw'>")
 
         # Генерируем строки таблицы с изменяющимся фоном
         for i in range(rows):
@@ -12,17 +12,21 @@ def generate_html_table(rows, output_file):
             background_color = f"rgb({color}, {color}, {color})"
 
             # Записываем строку с ячейкой и фоном
-            file.write(f"<tr style='background-color: {background_color};'><td>Row {i + 1}</td></tr>")
+            file.write(f"<tr style='background-color: {background_color};'><td>  </td></tr>")
 
         # Записываем конец таблицы и HTML-файла
         file.write("</table></body></html>")
 
-
-if __name__ == "__main__":
-    num_rows = 20  # Количество строк в таблице
+def table_creation():
+    num_rows = 500  # Количество строк в таблице
     output_filename = "table.html"  # Имя выходного HTML-файла
 
     # Генерируем HTML-файл с таблицей
     generate_html_table(num_rows, output_filename)
 
     print(f"HTML-файл с таблицей создан: {output_filename}")
+
+
+if __name__ == "__main__":
+    table_creation()
+    
